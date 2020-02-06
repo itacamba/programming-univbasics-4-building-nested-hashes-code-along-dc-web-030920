104cbfeb5347c2267fc7453b7f2c9fa86752a1fb
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-06 23:46:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,14 +3,22 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Montague",
+        :age  => "53"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague",
+        :age  => "54"
+        
         },
       :hero => {
         #your key/value pairs here
+        :name => "Romeo"
+        :Age => "15"
+        :status => "Alive"
         },
-      :hero_friends => []
+      :hero_friends => [{:name => "Benevolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {
